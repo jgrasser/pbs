8b0825cbd1a81b1832506ba7ce84a3544a39882f
Joseph Grasser <jgrasser.jge@gmail.com>
2020-07-14 20:28:46 -0700

    CLI flags should conform to hyphenated format instead of caml case or other

--- pbs/episodes.py
+++ pbs/episodes.py
@@ -3,12 +3,12 @@ import argparse, json
 
 def list(argv):
    parser = argparse.ArgumentParser(
-              usage='pbs.py episodes --show SHOW --seasoncid SEASONCID',
+              usage='pbs.py episodes --show SHOW --season-cid SEASON_CID',
               description='Search and play video content from PBS.org')
 
    parser.add_argument('--show', type=str, required=True)
-   parser.add_argument('--seasoncid', type=str, required=True)
+   parser.add_argument('--season-cid', type=str, required=True)
 
    args = parser.parse_args(argv)
 
-   return json.dumps(utils.client('GET', f'/show/{args.show}/seasons/{args.seasoncid}/episodes/?start=0&limit=24'))
+   return json.dumps(utils.client('GET', f'/show/{args.show}/seasons/{args.season_cid}/episodes/?start=0&limit=24'))
